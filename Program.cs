08aa89348f1903d707402b98ccf4b92a3f94ae49
Mariano Cardoso <marianocardoso92@gmail.com>
2024-09-21 00:46:52 -0300

    Primera Subida

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EJERCICIO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PASO 0: DECLARACION DE VARIABLES
+            int NUM;
+
+            //PASO 1: INGRESAR NUMERO
+            Console.WriteLine("Ingrese un numero: ");
+            NUM = int.Parse(Console.ReadLine());
+
+            //PASO 2: MOSTRAR AVISO
+
+            if(NUM > 10)
+                Console.WriteLine("El Numero es mayor a 10");
+            
+            else
+                Console.WriteLine("El numero no es mayor a 10");                    
+    
+        }
+    }
+}
